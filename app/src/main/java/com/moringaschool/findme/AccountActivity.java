d60b158cbb66e5ec881eb14f01043d55992775fe
karanikathungu-crypto <kathungudev@gmail.com>
2021-10-27 19:25:16 +0300

    "Add validation methods for creating account"

--- app/src/main/java/com/moringaschool/findme/AccountActivity.java
+++ app/src/main/java/com/moringaschool/findme/AccountActivity.java
@@ -1,41 +1,137 @@
 package com.moringaschool.findme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.textViewAccount) TextView mTextViewAccount;
-    @BindView(R.id.editTextUserName) EditText mEditTextUserName;
-    @BindView(R.id.editTextUserEmail) EditText mEditTextUserEmail;
-    @BindView(R.id.editTextUserPassword) EditText mEditTextUserPassword;
-    @BindView(R.id.createAccountButton) Button mCreateAccountButton;
+//    @BindView(R.id.textViewAccount) TextView mTextViewAccount;
+public static final String TAG = AccountActivity.class.getSimpleName();
+    @BindView(R.id.nameEditText) EditText mEditTextUserName;
+    @BindView(R.id.emailEditText) EditText mEditTextUserEmail;
+    @BindView(R.id.passwordEditText) EditText mEditTextUserPassword;
+    @BindView(R.id.confirmPasswordEditText) EditText mEditTextConfirmUserPassword;
+    @BindView(R.id.createUserButton) Button mCreateAccountButton;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        createAuthStateListener();
 
         ButterKnife.bind(this);
-
+        mAuth = FirebaseAuth.getInstance();
+        mLoginTextView.setOnClickListener(this);
         mCreateAccountButton.setOnClickListener(this);
 
 
     }
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(AccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 
     @Override
     public void onClick(View v) {
-        if (v == mCreateAccountButton) {
-            Toast.makeText(AccountActivity.this, "Account is being created", Toast.LENGTH_LONG).show();
+        if (v == mLoginTextView) {
+            Intent intent = new Intent(AccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
         }
+        if (v == mCreateAccountButton) {
+            createNewUser();
+        }
+    }
+        private void createNewUser() {
+            final String name = mEditTextUserName.getText().toString().trim();
+            final String email = mEditTextUserEmail.getText().toString().trim();
+            String password = mEditTextUserPassword.getText().toString().trim();
+            String confirmPassword = mEditTextConfirmUserPassword.getText().toString().trim();
+
+            boolean validEmail = isValidEmail(email);
+            boolean validName = isValidName(name);
+            boolean validPassword = isValidPassword(password, confirmPassword);
+            if (!validEmail || !validName || !validPassword) return;
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(AccountActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+    }
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEditTextUserEmail.setError("Please enter a valid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidName(String name) {
+        if (name.equals("")) {
+            mEditTextUserName.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mEditTextUserPassword.setError("Please create a password containing at least 6 characters");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mEditTextConfirmUserPassword.setError("Passwords do not match");
+            return false;
+        }
+        return true;
     }
 }
