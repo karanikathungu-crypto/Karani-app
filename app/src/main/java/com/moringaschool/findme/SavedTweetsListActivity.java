feaf7d81544f5ac7882182cce9b9ae80f2eb3d78
karanikathungu-crypto <kathungudev@gmail.com>
2021-10-27 18:38:26 +0300

    "Change the layout file reference to enable display of tweets"

---
+++ app/src/main/java/com/moringaschool/findme/SavedTweetsListActivity.java
@@ -0,0 +1,89 @@
+package com.moringaschool.findme;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SavedTweetsListActivity extends AppCompatActivity {
+    private DatabaseReference mRestaurantReference;
+    private FirebaseRecyclerAdapter<Datum, FirebaseTweetViewHolder> mFirebaseAdapter;
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+
+        mRestaurantReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_NAMES);
+        setUpFirebaseAdapter();
+        hideProgressBar();
+        showTweets();
+    }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Datum> options =
+                new FirebaseRecyclerOptions.Builder<Datum>()
+                        .setQuery(mRestaurantReference, Datum.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Datum, FirebaseTweetViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseTweetViewHolder firebaseTweetViewHolder, int position, @NonNull Datum tweet) {
+                firebaseTweetViewHolder.bindTweet(tweet);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseTweetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tweet_list_item, parent, false);
+                return new FirebaseTweetViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+
+    private void showTweets() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+}
